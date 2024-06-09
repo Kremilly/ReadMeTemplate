619277543871de2d33f1f462419bc1025fd16116
Kremilly <contact@kremilly.com>
2024-06-09 08:24:22 -0300

    chore: update file paths for static assets in render.html

--- index.py
+++ index.py
@@ -1,6 +1,15 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, Response
 
 app = Flask(__name__)
+
+def read_file_content(file_path: str) -> str:
+    with open(file_path, 'r') as file:
+        return file.read()
+
+@app.route('/raw/<file_path>')
+def read_file(file_path):
+    content = read_file_content(f'templates/{file_path}.html')
+    return Response(content, mimetype='text/plain')
 
 @app.route('/')
 def index():
