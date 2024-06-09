8ee5d024f58b4f4d553e45cde8b65604fb7dde3d
Kremilly <contact@kremilly.com>
2024-06-09 08:26:46 -0300

    chore: update index.py to render a different template for the /example route

--- index.py
+++ index.py
@@ -6,14 +6,14 @@ def read_file_content(file_path: str) -> str:
     with open(file_path, 'r') as file:
         return file.read()
 
-@app.route('/raw/<file_path>')
-def read_file(file_path):
-    content = read_file_content(f'templates/{file_path}.html')
-    return Response(content, mimetype='text/plain')
-
 @app.route('/')
 def index():
-    return render_template('render.html')
+    content = read_file_content(f'templates/render.html')
+    return Response(content, mimetype='text/plain')
+
+@app.route('/example')
+def example():
+    return render_template('rendered.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
